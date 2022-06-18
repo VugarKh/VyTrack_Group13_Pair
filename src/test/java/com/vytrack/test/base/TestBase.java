9afeaba04dea32d7dc152846916197b4118a41d2
vugar <vugar.khalil@gmail.com>
2022-06-18 18:29:03 -0400

    TestBase created

---
+++ src/test/java/com/vytrack/test/base/TestBase.java
@@ -0,0 +1,26 @@
+package com.vytrack.test.base;
+
+import com.vytrack.test.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.close();
+    }
+}
+
