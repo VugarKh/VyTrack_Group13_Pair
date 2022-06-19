40661e57ce17f9738e26fdd71c0f06d188732429
vugar <vugar.khalil@gmail.com>
2022-06-18 21:26:45 -0400

    Utilities created

---
+++ src/test/java/com/vytrack/test/utilities/VyTrack_Utilities.java
@@ -0,0 +1,22 @@
+package com.vytrack.test.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class VyTrack_Utilities {
+
+    public static void vyTrackLogin(WebDriver driver, String username, String password) {
+
+        driver.get("https://qa2.vytrack.com/");
+
+        WebElement userName = driver.findElement(By.id("prependedInput"));
+        userName.sendKeys(username);
+
+        WebElement passwordBox = driver.findElement(By.id("prependedInput2"));
+        passwordBox.sendKeys(password);
+
+        WebElement loginBtn = driver.findElement(By.id("_submit"));
+        loginBtn.click();
+    }
+}
