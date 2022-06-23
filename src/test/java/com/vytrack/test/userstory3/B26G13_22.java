a71e94b940ad21eeb72a06c614c1423d2444b68b
vugar <vugar.khalil@gmail.com>
2022-06-23 12:16:35 -0400

    final test

--- src/test/java/com/vytrack/test/userstory3/B26G13_22.java
+++ src/test/java/com/vytrack/test/userstory3/B26G13_22.java
@@ -3,6 +3,7 @@ package com.vytrack.test.userstory3;
 import com.vytrack.test.utilities.Driver;
 import com.vytrack.test.utilities.VyTrack_Utilities;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,6 +15,7 @@ public class B26G13_22 {
         VyTrack_Utilities.vyTrackLogin();
 
         WebElement fleetHeader = Driver.getDriver().findElement(By.xpath("//h1[@class='logo logo-text']"));
+        Thread.sleep(1000);
         String actualHeader = fleetHeader.getText();
         String expectedHeader = "FLEET MANAGEMENT";
 
@@ -25,7 +27,26 @@ public class B26G13_22 {
         WebElement vehicleOdometer = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-2'])[2]"));
         vehicleOdometer.click();
 
-        WebElement odometerRow = Driver.getDriver().findElement(By.xpath("//tr[@class='grid-row row-click-action']"));
+        Thread.sleep(1000);
+        WebElement filter = Driver.getDriver().findElement(By.xpath("//i[@class='fa-filter hide-text']"));
+        filter.click();
+
+        WebElement manageFilter = Driver.getDriver().findElement(By.xpath("//a[@class='add-filter-button']"));
+        manageFilter.click();
+
+        WebElement driverCheckBox = Driver.getDriver().findElement(By.xpath("//input[@id='ui-multiselect-0-0-option-2']"));
+        driverCheckBox.click();
+
+        WebElement driverAllBtn = Driver.getDriver().findElement(By.xpath("//div[@class='btn filter-criteria-selector" +
+                " oro-drop-opener oro-dropdown-toggle filter-default-value']"));
+        driverAllBtn.click();
+
+        WebElement searchDriver = Driver.getDriver().findElement(By.xpath("//input[@name='value']"));
+        searchDriver.sendKeys("Vugar" + Keys.ENTER);
+
+        Thread.sleep(1000);
+
+        WebElement odometerRow = Driver.getDriver().findElement(By.xpath("(//td[@data-column-label='Odometer Value'])[1]"));
         odometerRow.click();
 
         WebElement editBtn = Driver.getDriver().findElement(By.xpath("//a[@title='Edit Vehicle Odometer']"));
